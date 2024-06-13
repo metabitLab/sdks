57e7f56c5410d2381be8a9de8e79d67dce08e65e
lunacai <lunacai@126.com>
2024-06-13 16:06:58 +0800

    feat: update contract address

--- sdks/sdk-core/src/addresses.ts
+++ sdks/sdk-core/src/addresses.ts
@@ -78,10 +78,10 @@ export const V2_ROUTER_ADDRESSES: AddressMap = {
 
 const DEFAULT_ADDRESSES: ChainAddresses = {
   v3CoreFactoryAddress: '0x11cAC87e2c883862bDaa1738e5178cdeF1f46104',
-  multicallAddress: '',
-  quoterAddress: '',
+  multicallAddress: '0x2cCC6635E78E841D0A480fAdbee716fd11E8417b',
+  quoterAddress: '0x64f7E1F83a1fC38f15DFb74f2a8e1CB2d97B033F',
   v3MigratorAddress: '0xcDc10593a66185AAa206665C5083ac51Ad935F91',
-  nonfungiblePositionManagerAddress: '',
+  nonfungiblePositionManagerAddress: '0x8e46DefA1B20Fba826340d8d1a5Ba0dd2fa1e611',
 }
 
 const MAINNET_ADDRESSES: ChainAddresses = {
